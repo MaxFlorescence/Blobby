96a73f9637f516ce975e19a7d779efa82a29a951
MaxFlorescence <maxflorescence@gmail.com>
2025-09-10 16:52:48 -0600

    Begin refactoring how AtomControllers do work

--- Assets/Scripts/Blob/AtomController.cs
+++ Assets/Scripts/Blob/AtomController.cs
@@ -1,23 +1,24 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Assertions;
 
+/// <summary>
+///    This class defines the behavior of each individual atom in the blob.
+/// </summary>
 public class AtomController : MonoBehaviour
 {
-    private Rigidbody rigidBody;
-    private HashSet<GameObject> touching = new HashSet<GameObject>();
     public BlobController blobController;
+    public Squisher squisher;
+    public Vector3 force;
+    public Vector3 impulse;
     public int touchCount = 0;
 
-    public Vector3 moveForce;
-    public Vector3 jumpForce;
-    public Squisher squisher;
+    private Rigidbody rigidBody;
+    private HashSet<GameObject> touching = new HashSet<GameObject>();
 
     void Start()
     {
-        // contactPoints = new ArrayList(10);
         rigidBody = GetComponent<Rigidbody>();
         squisher = blobController.GetComponent<Squisher>();
     }
@@ -25,88 +26,78 @@ public class AtomController : MonoBehaviour
     void FixedUpdate()
     {
         Assert.IsTrue(touchCount >= 0);
-        
-        rigidBody.AddForce(moveForce, ForceMode.Force);
-        rigidBody.AddForce(jumpForce, ForceMode.Impulse);
+
+        rigidBody.AddForce(force, ForceMode.Force);
+        rigidBody.AddForce(impulse, ForceMode.Impulse);
+        impulse = Vector3.zero;
     }
 
-    public void useGravity(bool use) {
+    public void useGravity(bool use)
+    {
         if (rigidBody != null)
             rigidBody.useGravity = use;
     }
 
-    // void ApplyJump() {
-    //     Vector3 jumpDirection = Vector3.zero;
-
-    //     // foreach (ContactPoint contact in contactPoints)
-    //     //     jumpDirection += contact.normal;
-        
-    // }
-
-    void OnCollisionEnter(Collision collision) {
+    void OnCollisionEnter(Collision collision)
+    {
         GameObject obj = collision.gameObject;
 
-        if(NotAtom(obj)) {
+        if (NotAtom(obj))
+        {
             squisher.squish();
 
-            if(NotBounds(obj) && !touching.Contains(obj)) {
+            if (NotBounds(obj) && !touching.Contains(obj))
+            {
                 touchedHazard(obj);
                 touchedGoal(obj);
-                touchedCannon(obj);
+                Interactable interactableObj = obj.GetComponent<Interactable>();
+                if (interactableObj != null)
+                {
+                    interactableObj.OnInteract(blobController);
+                }
 
-                if (!touchedFlag(obj)) {
+                if (!blobController.TryToGrab(obj))
+                { // don't count grabbed objects as touching
                     touching.Add(obj);
                     touchCount++;
                 }
             }
         }
-        // for (int i = 0; i < collision.contactCount; i++)
-        //     contactPoints.Add(collision.GetContact(i));
     }
 
-    void touchedHazard(GameObject obj) {
-        if (obj.tag == "Hazard") {
-            blobController.lose();
+    void touchedHazard(GameObject obj)
+    {
+        if (obj.tag == "Hazard")
+        {
+            blobController.Lose();
         }
     }
 
-    bool touchedFlag(GameObject obj) {
-        if (obj.tag == "Flag") {
-            blobController.grabFlag(obj);
-            return true;
-        }
-
-        return false;
-    }
-
-    void touchedCannon(GameObject obj) {
-        if (obj.tag == "Cannon") {
-            obj.GetComponent<Cannon>().Insert(blobController);
+    void touchedGoal(GameObject obj)
+    {
+        if (obj.tag == "Start Platform")
+        {
+            blobController.Win(obj);
         }
     }
 
-    void touchedGoal(GameObject obj) {
-        if (obj.tag == "Start Platform") {
-            blobController.win(obj);
-        }
-    }
-
-    void OnCollisionExit(Collision collision) {
+    void OnCollisionExit(Collision collision)
+    {
         GameObject obj = collision.gameObject;
-        if(NotAtom(obj) && NotBounds(obj) && touching.Contains(obj)) {
+        if (NotAtom(obj) && NotBounds(obj) && touching.Contains(obj))
+        {
             touching.Remove(obj);
             touchCount--;
         }
-        // for (int i = 0; i < collision.contactCount; i++)
-        //     contactPoints.Remove(collision.GetContact(i));
     }
 
-    bool NotAtom(GameObject obj) {
-        // return obj.tag != "Atom";
+    bool NotAtom(GameObject obj)
+    {
         return !blobController.blobAtoms.Contains(obj);
     }
 
-    bool NotBounds(GameObject obj) {
+    bool NotBounds(GameObject obj)
+    {
         return obj.tag != "Bounds";
     }
 }
