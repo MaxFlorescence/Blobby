a0581f40ed41d89a16c843374c8ca16cacaa636f
MaxFlorescence <maxflorescence@gmail.com>
2025-09-10 17:48:06 -0600

    Documentation for interactable interface

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,4 +1,13 @@
+/// <summary>
+///     An interface for objects that can be interacted with by the blob character.
+/// </summary>
 public interface Interactable
 {
+    /// <summary>
+    ///    Called by a blob character when it interacts with this object.
+    /// </summary>
+    /// <param name="blob">
+    ///     The blob character interacting with this object.
+    /// </param>
     public abstract void OnInteract(BlobController blob);
 }
