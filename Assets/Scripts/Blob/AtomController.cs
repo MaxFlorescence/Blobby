062b5191a182b3584f455ccf31fb514febf32bbb
MaxFlorescence <maxflorescence@gmail.com>
2025-09-10 17:46:28 -0600

    Documentation and a bit of refactoring for AtomController

--- Assets/Scripts/Blob/AtomController.cs
+++ Assets/Scripts/Blob/AtomController.cs
@@ -8,21 +8,29 @@ using UnityEngine.Assertions;
 /// </summary>
 public class AtomController : MonoBehaviour
 {
+    // Public members
     public BlobController blobController;
     public Squisher squisher;
     public Vector3 force;
     public Vector3 impulse;
     public int touchCount = 0;
 
+    // Private members
     private Rigidbody rigidBody;
     private HashSet<GameObject> touching = new HashSet<GameObject>();
 
+    /// <summary>
+    ///     Initialize components.
+    /// </summary>
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         squisher = blobController.GetComponent<Squisher>();
     }
 
+    /// <summary>
+    ///     Apply forces to the atom. Impulse forces reset immediately.
+    /// </summary>
     void FixedUpdate()
     {
         Assert.IsTrue(touchCount >= 0);
@@ -32,19 +40,31 @@ public class AtomController : MonoBehaviour
         impulse = Vector3.zero;
     }
 
+    /// <summary>
+    ///    Enable or disable gravity for this atom.
+    /// </summary>
+    /// <param name="gravity">
+    ///     <tt>true</tt> enables gravity, <tt>false</tt> disables it.
+    /// </param>
     public void SetGravity(bool gravity)
     {
         rigidBody.useGravity = gravity;
     }
 
+    /// <summary>
+    ///     Handle activating interactable objects, grabbable objects, and tracking the touch count.
+    /// </summary>
     void OnCollisionEnter(Collision collision)
     {
         GameObject obj = collision.gameObject;
 
-        if (NotAtom(obj))
-        {
+        if (NotMyAtoms(obj))
+        { // do nothing special when colliding with other atoms
             squisher.squish();
 
+            // Don't interact with boundaries or already-touched objects.
+            // Boundaries do not affect the touch count to prevent the blob from moving solely by
+            // touching them. This prevents players from skipping sections by moving along the boundaries.
             if (NotBounds(obj) && !touching.Contains(obj))
             {
                 Interactable interactableObj = obj.GetComponent<Interactable>();
@@ -62,23 +82,44 @@ public class AtomController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    ///     Keep track of the touch count.
+    /// </summary>
     void OnCollisionExit(Collision collision)
     {
         GameObject obj = collision.gameObject;
-        if (NotAtom(obj) && NotBounds(obj) && touching.Contains(obj))
+        if (NotMyAtoms(obj) && NotBounds(obj) && touching.Contains(obj))
         {
             touching.Remove(obj);
             touchCount--;
         }
     }
 
-    bool NotAtom(GameObject obj)
+    /// <summary>
+    ///     Return true if the given object is not one of this blob's atoms.
+    /// </summary>
+    /// <param name="obj">
+    ///     The GameObject to check.
+    /// </param>
+    /// <returns>
+    ///     <tt>false</tt> iff the object is one of this blob's atoms.
+    /// </returns>
+    bool NotMyAtoms(GameObject obj)
     {
         return !blobController.blobAtoms.Contains(obj);
     }
 
+    /// <summary>
+    ///    Return true if the given object is not a boundary object.
+    /// </summary>
+    /// <param name="obj">
+    ///     The GameObject to check.
+    /// </param>
+    /// <returns>
+    ///     <tt>false</tt> iff the object is a boundary object.
+    /// </returns>
     bool NotBounds(GameObject obj)
     {
-        return obj.tag != "Bounds";
+        return !obj.CompareTag("Bounds");
     }
 }
