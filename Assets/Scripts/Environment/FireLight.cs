0a6fbbcc4e89f87160a0c83f517a1e98a3f9e85c
MaxFlorescence <maxflorescence@gmail.com>
2025-12-04 19:27:08 -0700

    Add torch object

---
+++ Assets/Scripts/Environment/FireLight.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+class FireLight : MonoBehaviour
+{
+    public Vector2 intervalRange = new(0f, 1f);
+    public Vector2 intensityRange = new(0.5f, 1f);
+    public float maxDisplacement = 0.25f;
+
+    public bool IsOn { get; set; } = true;
+
+    private Light fireLight;
+    float targetIntensity;
+    float lastIntensity;
+    float interval;
+    float timer;
+
+    Vector3 targetPosition;
+    Vector3 lastPosition;
+    Vector3 origin;
+
+    private void Start()
+    {
+        fireLight = GetComponent<Light>();
+        origin = transform.position;
+        lastPosition = origin;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > interval)
+        {
+            lastIntensity = fireLight.intensity;
+            targetIntensity = NextIntensity();
+            timer = 0;
+            interval = Random.Range(intervalRange.x, intervalRange.y);
+
+            lastPosition = fireLight.transform.position;
+            targetPosition = NextPosition();
+        }
+
+        fireLight.intensity = Mathf.Lerp(lastIntensity, targetIntensity, timer / interval);
+        fireLight.transform.position = Vector3.Lerp(lastPosition, targetPosition, timer / interval);
+    }
+
+    public float NextIntensity()
+    {
+        if (IsOn)
+            return Random.Range(intensityRange.x, intensityRange.y);
+
+        return 0;
+    }
+
+    public Vector3 NextPosition()
+    {
+        if (IsOn)
+            return origin + Random.insideUnitSphere * maxDisplacement;
+
+        return origin;
+    }
+}
---
+++ Assets/Scripts/Interactions/Fire.cs
@@ -0,0 +1,94 @@
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+/// <summary>
+///     Controls effects of fires being ignited and extinguished.
+/// </summary>
+public class Fire : Interactable
+{
+    public GameObject flames;
+    public ParticleSystem particles;
+    public Light pointLight;
+    public float fadeTime = 1;
+
+    private float timer = 0;
+    private float targetStrength = 1;
+    private float lastStrength = 1;
+    private FireLight fireLight;
+    private Renderer[] fireRenderers;
+    private AudioSource audioSource;
+    private AudioClip fireIgnite, fireSizzle;
+
+    void Start()
+    {
+        fireLight = pointLight.GetComponent<FireLight>();
+        audioSource = GetComponent<AudioSource>();
+        fireIgnite = Resources.Load("Sounds/fire_ignite", typeof(AudioClip)) as AudioClip;
+        fireSizzle = Resources.Load("Sounds/fire_sizzle", typeof(AudioClip)) as AudioClip;
+
+        fireRenderers = flames.GetComponentsInChildren<Renderer>().ToArray();
+    }
+
+    protected override void OnUpdate()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            SetFlameStrength(Mathf.Lerp(targetStrength, lastStrength, timer / fadeTime));
+        }
+        else if (lastStrength != targetStrength)
+        {
+            SetFlameStrength(targetStrength);
+            timer = fadeTime;
+        }
+
+    }
+
+    private void SetFlameStrength(float newStrength)
+    {
+        foreach (Renderer renderer in fireRenderers)
+        {
+            renderer.material.SetFloat("_Strength", newStrength);
+        }
+
+        lastStrength = newStrength;
+    }
+
+    /// <summary>
+    ///     Put out the fire on interaction
+    /// </summary>
+    protected override void OnInteract(BlobController blob)
+    {
+        Extinguish();
+        StartInteractionCooldown(10);
+    }
+
+    protected override void OnInteractionCooldownEnd()
+    {
+        Ignite();
+    }
+
+    private void Ignite()
+    {
+        FadeStrengthTo(1);
+        particles.Play();
+        fireLight.IsOn = true;
+        audioSource.PlayOneShot(fireIgnite);
+    }
+
+    private void Extinguish()
+    {
+        FadeStrengthTo(0);
+        particles.Stop();
+        fireLight.IsOn = false;
+        audioSource.PlayOneShot(fireSizzle);
+    }
+
+    private void FadeStrengthTo(float newStrength)
+    {
+        targetStrength = newStrength;
+        timer = fadeTime;
+    }
+}
