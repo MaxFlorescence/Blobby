7df1c60627874227b19826239aafa9cc4e49c871
MaxFlorescence <maxflorescence@gmail.com>
2026-03-09 15:44:27 -0600

    Document squisher.

--- Assets/Scripts/Sounds/Squisher.cs
+++ Assets/Scripts/Sounds/Squisher.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 
 public class Squisher : MonoBehaviour
 {
+    /// <summary>
+    ///     The audioSource set by the corresponding blob controller.
+    /// </summary>
     public AudioSource audioSource;
-    public AudioClip[] clips;
+    /// <summary>
+    ///     The list of possible squish noises that can be played.
+    /// </summary>
+    private AudioClip[] clips;
+    private const int CLIP_COUNT = 8;
 
     void Awake() {
-        clips = new AudioClip[8];
+        clips = new AudioClip[CLIP_COUNT];
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < CLIP_COUNT; i++) {
             clips[i] = Resources.Load("Sounds/squish_" + i.ToString(), typeof(AudioClip)) as AudioClip;
         }
     }
@@ -18,6 +25,9 @@ public class Squisher : MonoBehaviour
         audioSource.volume = 0.05f;
     }
 
+    /// <summary>
+    ///     Play a random squish noise with a random pitch.
+    /// </summary>
     public void squish() {
         if (!GameInfo.StartCutscene) {
             audioSource.pitch = Random.Range(0.5f, 1.5f);
