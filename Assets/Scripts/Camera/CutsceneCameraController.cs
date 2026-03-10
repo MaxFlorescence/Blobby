cdbed87e602a8f358bb72278c4ec34dac63e0593
MaxFlorescence <maxflorescence@gmail.com>
2026-03-10 10:27:44 -0600

    Refactor and document CutsceneCameraController.

--- Assets/Scripts/Camera/CutsceneCameraController.cs
+++ Assets/Scripts/Camera/CutsceneCameraController.cs
@@ -1,37 +1,56 @@
 using UnityEngine;
 
+/// <summary>
+///     A struct defining a keyframe for cutscene cameras.
+/// </summary>
+public readonly struct CameraKeyFrame
+{
+    /// <summary>
+    ///     The position of the camera at this keyframe.
+    /// </summary>
+    public readonly Vector3 position;
+    /// <summary>
+    ///     The (normalized) orientation of the camera at this keyframe.
+    /// </summary>
+    public readonly Vector3 orientation;
+    /// <summary>
+    ///     The amount of time it takes to move from the previous keyframe to this one.
+    /// </summary>
+    public readonly float moveTime;
+    /// <summary>
+    ///     The amount of time to pause for after moving to this keyframe, but before moving to the
+    ///     next keyframe.
+    /// </summary>
+    public readonly float pauseTime;
+
+    public CameraKeyFrame(Vector3 position, Vector3 orientation, float pauseTime, float moveTime)
+    {
+        this.position = position;
+        this.orientation = orientation.normalized;
+        this.pauseTime = pauseTime;
+        this.moveTime = moveTime;
+    }
+}
+
+/// <summary>
+///     A class for controlling a camera's movement using a sequence of POVs.
+/// </summary>
 public class CutsceneCameraController : PriorityCamera
 {
-    public GameObject skipOverlay;
-
-    private float[] pauseTimes = {
-        2f,
-        1.5f,
-        1f,
-        0.5f
+    public CameraKeyFrame[] keyFrames =
+    {
+        new(new(-30, 40, -47), new(0, 0, -1),  2f,   0f),
+        new(new(-40, 30, -40), new(-1, 0, -1), 1.5f, 2f),
+        new(new(47, 30, 47),   new(-1, 0, -1), 1f,   4f),
+        new(new(47, 30, 47),   new(0, -1, 0),  0.5f, 2f)
     };
-    private float[] moveTimes = {
-        0f,
-        2f,
-        4f,
-        2f
-    };
-    private Vector3[] positions = {
-        new Vector3(-30, 40, -47),
-        new Vector3(-40, 30, -40),
-        new Vector3(47, 30, 47),
-        new Vector3(47, 30, 47)
-    };
-    private Vector3[] directions = {
-        new Vector3(0, 0, -1),
-        new Vector3(-1, 0, -1),
-        new Vector3(-1, 0, -1),
-        new Vector3(0, -1, 0)
-    };
-
-    private int points;
-    private int currentPoint = -1;
-    private float t = 0;
+    /// <summary>
+    ///     The canvas overlay to display during the cutscene.
+    /// </summary>
+    public GameObject cutsceneOverlay;
+    private int keyFrameCount;
+    private int currentKeyFrame = -1;
+    private float cutsceneTime = 0;
 
     void Awake()
     {
@@ -40,13 +59,9 @@ public class CutsceneCameraController : PriorityCamera
 
     void Start()
     {
-        points = positions.Length;
-        foreach (Vector3 direction in directions)
-        {
-            direction.Normalize();
-        }
+        keyFrameCount = keyFrames.Length;
 
-        // this flag is only true when starting the game from the main menu
+        // this flag might be true only when starting the game from the main menu
         if (GameInfo.StartCutscene)
         {
             BeginCutscene();
@@ -59,28 +74,28 @@ public class CutsceneCameraController : PriorityCamera
 
     override protected void OnActivate()
     {
-        skipOverlay.SetActive(true);
+        cutsceneOverlay.SetActive(true);
     }
 
     override protected void OnDeactivate()
     {
-        skipOverlay.SetActive(false);
+        cutsceneOverlay.SetActive(false);
     }
 
     public void BeginCutscene()
     {
         SetPriority(2);
         
-        currentPoint = 0;
-        transform.position = positions[0];
-        transform.LookAt(positions[0] + directions[0]);
+        currentKeyFrame = 0;
+        transform.position = keyFrames[0].position;
+        transform.LookAt(keyFrames[0].position + keyFrames[0].orientation);
     }
 
     void EndCutscene()
     {
         SetPriority(0);
         
-        currentPoint = -1;
+        currentKeyFrame = -1;
         GameInfo.StartCutscene = false;
     }
 
@@ -93,30 +108,40 @@ public class CutsceneCameraController : PriorityCamera
         }
     }
 
+    /// <summary>
+    ///     For the first <tt>moveTime</tt> seconds of a keyframe, interpolate the camera to its
+    ///     position and orientation.
+    ///     <br/>
+    ///     For the next <tt>pauseTime</tt> seconds of a keyframe, do nothing.
+    ///     <br/>
+    ///     After these time periods, start the next keyframe.
+    ///     <br/>
+    ///     After all keyframes are finished, end the cutscene.
+    /// </summary>
     void LateUpdate()
     {
-        if (currentPoint < points && currentPoint >= 0)
+        if (currentKeyFrame < keyFrameCount && currentKeyFrame >= 0)
         {
-            if (moveTimes[currentPoint] > 0 && t <= moveTimes[currentPoint])
+            if (keyFrames[currentKeyFrame].moveTime > 0 && cutsceneTime <= keyFrames[currentKeyFrame].moveTime)
             {
                 // interpolate position linearly between the last position and the target position
-                Vector3 position = Vector3.Lerp(positions[currentPoint - 1], positions[currentPoint], t / moveTimes[currentPoint]);
+                Vector3 position = Vector3.Lerp(keyFrames[currentKeyFrame - 1].position, keyFrames[currentKeyFrame].position, cutsceneTime / keyFrames[currentKeyFrame].moveTime);
                 // interpolate direction spherically between the last direction and the target direction
-                Vector3 direction = Vector3.Slerp(directions[currentPoint - 1], directions[currentPoint], t / moveTimes[currentPoint]);
+                Vector3 direction = Vector3.Slerp(keyFrames[currentKeyFrame - 1].orientation, keyFrames[currentKeyFrame].orientation, cutsceneTime / keyFrames[currentKeyFrame].moveTime);
 
                 transform.position = position;
                 transform.LookAt(position + direction);
             }
-            else if (t >= moveTimes[currentPoint] + pauseTimes[currentPoint])
+            else if (cutsceneTime >= keyFrames[currentKeyFrame].moveTime + keyFrames[currentKeyFrame].pauseTime)
             {
-                // interpolations finished
-                t = 0;
-                currentPoint++;
+                // interpolation is finished
+                cutsceneTime = 0;
+                currentKeyFrame++;
             }
 
-            t += Time.deltaTime;
+            cutsceneTime += Time.deltaTime;
         }
-        else if (currentPoint == points)
+        else if (currentKeyFrame == keyFrameCount)
         {
             EndCutscene();
         }
