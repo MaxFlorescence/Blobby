e8c6816ec60dd470a9520328cb1301909aa6bc1e
MaxFlorescence <maxflorescence@gmail.com>
2026-04-15 20:26:24 -0600

    Update cannon object and script to be awesome

--- Assets/Scripts/Interactions/Objects/Cannon.cs
+++ Assets/Scripts/Interactions/Objects/Cannon.cs
@@ -1,55 +1,100 @@
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
-[RequireComponent(typeof(SphereCollider))]
-public class Cannon : Interactable
-{
-    // Properties  
-    /// <summary>
-    ///     The direction the cannon barrel faces.
-    /// </summary>
-    private Vector3 direction = Vector3.up * 3.7f + Vector3.forward * 6.5f;
-    /// <summary>
-    ///    The position of the cannon center.
-    /// </summary>
-    private Vector3 cannonCenter = Vector3.up * 2.3f;
-    /// <summary>
-    ///    The speed at which the cannon rotates (degrees per second).
-    /// </summary>
-    private float angularSpeed = 80;
+[RequireComponent(typeof(Rigidbody))]
 
-    // Exiting
+/// <summary>
+///     A class for controlling the actions of a cannon object.
+/// </summary>
+public class Cannon : Interactable, Controllable
+{
+    //----------------------------------------------------------------------------------------------
+    // Components
+    //----------------------------------------------------------------------------------------------
+    private Rigidbody cannonRigidBody;
+    public Transform barrelTransform;
+    private AudioSource audioSource;
+    /// <summary>
+    ///     Used to apply a cooldown between firing or loading actions
+    /// </summary>
+    private Timer controlCooldownTimer;
+
+    //----------------------------------------------------------------------------------------------
+    // Aiming
+    //----------------------------------------------------------------------------------------------
+    public bool controlled { get; set; } = false;
+    /// <summary>
+    ///     The angle that the cannon's barrel makes with the horizontal.
+    /// </summary>
+    private float angle = 0;
+    /// <summary>
+    ///     The minimum and maximum angles that the barrel can make with the horizontal.
+    /// </summary>
+    private readonly Vector2 ANGLE_BOUNDS = new(-30, 30);
+    /// <summary>
+    ///     How fast the barrel rotates during aiming.
+    /// </summary>
+    private float angularSpeedFactor = 0.5f;
+
+    //----------------------------------------------------------------------------------------------
+    // Loading
+    //----------------------------------------------------------------------------------------------
+    /// <summary>
+    ///     The position at which ammo sits in the barrel.
+    /// </summary>
+    public Transform ammoPlaceholder;
+    /// <summary>
+    ///     The inventory that holds ammo.
+    /// </summary>
+    private Inventory barrel;
+    /// <summary>
+    ///     The blob currently in the cannon.
+    /// </summary>
+    private BlobController blob = null;
+    /// <summary>
+    ///     The name of the audio clip to play when ammo is loaded into the cannon.
+    /// </summary>
+    private const string LOAD_AUDIO = "cannon_load";
+    /// <summary>
+    ///     The factor by which a loaded blob is squished to fit into the cannon.
+    /// </summary>
+    private const float LOADED_BLOB_SIZE_FACTOR = 0.3f;
+
+    //----------------------------------------------------------------------------------------------
+    // Firing  
+    //----------------------------------------------------------------------------------------------
+    /// <summary>
+    ///     The location of the barrel's muzzle.
+    /// </summary>
+    public Transform muzzlePlaceholder;
     /// <summary>
     ///    The force magnitude with which to fire the cannon.
     /// </summary>
-    private float firePower = 32f;
+    public float firePower = 32f;
+    /// <summary>
+    ///     The multiplier to apply to the firing force when firing non-blob ammo.
+    /// </summary>
+    private float ammoForceFactor = 10f;
     /// <summary>
     ///    The force magnitude with which to exit the cannon.
     /// </summary>
     private float cancelPower = 2f;
-
-    // Loading
-    private bool cannonLoaded = false;
     /// <summary>
-    ///     The blob currently in the cannon, or null if none.
+    ///     The audio clip to play when the cannon fires.
     /// </summary>
-    private BlobController blob = null;
-    /// <summary>
-    ///     The position the blob entered the cannon from.
-    /// </summary>
-    private Vector3 entryPosition = Vector3.zero;
-
-    // Components
-    /// <summary>
-    ///     The audio source to play when the cannon fires.
-    /// </summary>
-    private AudioSource cannonFireAudio;
-    private SphereCollider cannonCollider;
+    private AudioClip fireAudioClip;
+    private const string FIRE_AUDIO = "cannon_fire";
 
     void Start(){
-        cannonCenter += transform.position;
-        cannonFireAudio = GetComponent<AudioSource>();
-        cannonCollider = GetComponent<SphereCollider>();
+        audioSource = gameObject.AddComponent<AudioSource>();
+        cannonRigidBody = GetComponent<Rigidbody>();
+        
+        barrel = gameObject.AddComponent<Inventory>();
+        barrel.SetCapacity(1);
+        barrel.SetDisplayPositionCallback(() => ammoPlaceholder.position);
+        barrel.SetAudio(LOAD_AUDIO);
+        fireAudioClip = Utilities.LoadAudioClip(FIRE_AUDIO);
+
+        controlCooldownTimer = new(1.5f);
     }
 
     /// <summary>
@@ -57,21 +102,44 @@ public class Cannon : Interactable
     /// </summary>
     protected override void OnUpdate()
     {
-        float angle = angularSpeed * Time.deltaTime;
-        transform.Rotate(Vector3.up, angle);
-        direction = Quaternion.AngleAxis(angle, Vector3.up) * direction;
-
-        if (cannonLoaded)
+        if (blob != null && blob.ghostMode)
         {
-            if (Input.GetButtonDown("Jump"))
-            {
-                Fire();
-            }
-            else if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                Cancel();
-            }
+            StartInteractionCooldown(0.5f);
+            controlled = false;
+            blob.SetControlCanRelease(true);
+            gameObject.SetLayer(Utilities.DEFAULT_LAYER);
+            blob = null;   
+        }
 
+        if (blob != null) blob.Teleport(ammoPlaceholder.position);
+
+        if (!controlled) return;
+        
+        float verticalInput = Input.GetAxis("Vertical");
+        if (verticalInput != 0) AimBarrel(verticalInput * angularSpeedFactor);
+
+        if (!controlCooldownTimer.Update(reset: false)) return;
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (barrel.IsEmpty())
+            { // load ammo
+                barrel.TryTakeFrom(blob.inventory);
+            }
+            else
+            { // unload ammo
+                barrel.TryGiveTo(blob.inventory);
+            }
+            controlCooldownTimer.Reset();
+        }
+        else if (Input.GetButtonDown("Jump"))
+        {
+            Fire();
+            controlCooldownTimer.Reset();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Eject();
         }
     }
 
@@ -80,66 +148,105 @@ public class Cannon : Interactable
     /// </summary>
     protected override void OnInteract(BlobController blob)
     {
-        if (!cannonLoaded)
-        {
-            cannonCollider.isTrigger = true;
-            cannonLoaded = true;
-            entryPosition = blob.GetPosition();
-            this.blob = blob;
+        if (this.blob != null || !blob.IsSticky()) return;
+        
+        Reorient();
 
-            blob.SetMovementInputEnabled(false);
-            blob.SetGravity(false);
-            blob.Teleport(cannonCenter);
-            blob.ApplyForces(Vector3.zero, Vector3.zero, false);
-            blob.HoldCenterAtom(true);
-            blob.OverrideSpringLengths(1f);
+        this.blob = blob;
+        controlled = true;
 
-            SetInteractionEnabled(false);
-        }
+        blob.SetControlCanRelease(false);
+        blob.SetRestrained(true, LOADED_BLOB_SIZE_FACTOR);
+
+        gameObject.SetLayer(Utilities.IGNORE_CAMERA_LAYER);
+        SetInteractionEnabled(false);
+
+        AllowFreeRotation(false);
     }
 
     /// <summary>
-    ///    Fire the blob out of the cannon in the direction the cannon is facing.
+    ///    Fire the ammo or blob out of the cannon in the direction the cannon is facing.
     /// </summary>
     public void Fire()
     {
-        if (cannonLoaded)
-        {
-            blob.Teleport(cannonCenter + direction);
-            blob.SetGravity(true);
-            blob.ApplyForces(null, firePower * direction.normalized, false);
-            blob.SetMovementInputEnabled(true, 0.5f);
-            blob.HoldCenterAtom(false);
-            blob.RestoreSpringLengths();
+        Vector3 firePosition = muzzlePlaceholder.position;
+        Vector3 fireForce = firePower * GetFireDirection();
 
-            blob = null;
-            cannonLoaded = false;
-            SetInteractionEnabled(true);
+        if (!barrel.IsEmpty())
+        {
+            Grip ammo = barrel.GetObject().GetComponent<Grip>();
+            ammo.TryLeaveInventory(true, firePosition, fireForce*ammoForceFactor);
+        }
+        else if (blob != null)
+        {
+            RemoveBlob(firePosition, fireForce);
         }
 
-        cannonFireAudio.Play();
-        cannonCollider.isTrigger = false;
+        audioSource.PlayOneShot(fireAudioClip);
     }
 
     /// <summary>
     ///     Eject the currently inserted blob from the cannon without firing.
     /// </summary>
-    public void Cancel()
+    public void Eject()
     {
-        if (cannonLoaded)
+        if (blob == null) return;
+        
+        Vector3 worldEjectPosition = muzzlePlaceholder.position;
+        Vector3 cancelForce = cancelPower * (worldEjectPosition - ammoPlaceholder.position).normalized;
+        RemoveBlob(worldEjectPosition, cancelForce);
+    }
+
+    private void RemoveBlob(Vector3 exitPosition, Vector3 exitForce, bool resetAngle = false)
+    {
+        if (resetAngle) AimBarrel();
+
+        StartInteractionCooldown(3f);
+        controlled = false;
+        gameObject.SetLayer(Utilities.DEFAULT_LAYER);
+
+        blob.SetControlCanRelease(true);
+        blob.Teleport(exitPosition);
+        blob.ApplyForces(null, exitForce, false);
+
+        blob.SetRestrained(false, delaySpringUnlock: 0.25f);
+
+        blob = null;
+        AllowFreeRotation(true);
+    }
+
+    private Vector3 GetFireDirection()
+    {
+        return (muzzlePlaceholder.position - ammoPlaceholder.position).normalized;
+    }
+
+    private void Reorient(bool resetAngle = false)
+    {
+        Vector3 aimDir = Vector3.ProjectOnPlane(
+            muzzlePlaceholder.position - transform.position, Vector3.up
+        );
+
+        if (resetAngle) AimBarrel();
+
+        transform.LookAt(aimDir + transform.position, Vector3.up);
+        transform.Translate(Vector3.up * 0.1f);
+    }
+
+    private void AimBarrel(float angleDelta = 0)
+    {
+        if (angleDelta == 0) angleDelta = -angle;
+
+        if (!(angle + angleDelta).OutOfBounds(ANGLE_BOUNDS.y, ANGLE_BOUNDS.x))
         {
-            blob.Teleport(entryPosition);
-            blob.SetGravity(true);
-            blob.ApplyForces(null, cancelPower * (entryPosition - cannonCenter).normalized, false);
-            blob.SetMovementInputEnabled(true, 0.5f);
-            blob.HoldCenterAtom(false);
-            blob.RestoreSpringLengths();
-
-            blob = null;
-            cannonLoaded = false;
-            StartInteractionCooldown(3f);
+            angle += angleDelta;
+            barrelTransform.Rotate(Vector3.forward, angleDelta);
         }
+    }
 
-        cannonCollider.isTrigger = false;
+    private void AllowFreeRotation(bool allow)
+    {
+        cannonRigidBody.constraints = allow ?
+            RigidbodyConstraints.None :
+            RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
     }
 }
