1ffda5f285f49e02a9c24e75b2dfe60ea211f0fd
MaxFlorescence <maxflorescence@gmail.com>
2026-04-29 15:46:15 -0600

    Document MiniMap.cs

--- Assets/Scripts/UI/MiniMap.cs
+++ Assets/Scripts/UI/MiniMap.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+///     A class defining the minimap of the game.
+/// </summary>
 [RequireComponent(typeof(RectTransform))]
 public class MiniMap : MonoBehaviour
 {
-    public GameObject playerIcon;
-    public GameObject tileCanvas;
-    public GameObject mapBorder;
-
+    // ---------------------------------------------------------------------------------------------
+    // GRAPHICS
+    // ---------------------------------------------------------------------------------------------
     private Material MAP_ICON_MATERIAL;
+    public GameObject playerIcon;
+    public GameObject mapBorder;
+    /// <summary>
+    ///     The canvas on which minimap icons are drawn.
+    /// </summary>
+    public GameObject tileCanvas;
+
+    // ---------------------------------------------------------------------------------------------
+    // DIMENSIONS
+    // ---------------------------------------------------------------------------------------------
     private RectTransform rectTransform;
+    /// <summary>
+    ///     The position of the minimap's center point on the screen, with inverted y coordinate.
+    /// </summary>
     private Vector3 mapScreenPosition;
 
     void Awake()
@@ -49,6 +64,28 @@ public class MiniMap : MonoBehaviour
         mapBorder.GetComponent<RectTransform>().rotation = viewRotation;
     }
 
+    /// <summary>
+    ///     Creates a new minimap icon gameobject with the corresponding parameters.
+    /// </summary>
+    /// <param name="name">
+    ///     The name of the minimap icon.
+    /// </param>
+    /// <param name="sprite">
+    ///     The sprite to use for the minimap icon.
+    /// </param>
+    /// <param name="position">
+    ///     The position of the icon on the minimap, as if it were a 1x1 square with origin at its
+    ///     center point.
+    /// </param>
+    /// <param name="rotation">
+    ///     The rotation of the icon on the minimap.
+    /// </param>
+    /// <param name="scale">
+    ///     The scale of the icon on the minimap.
+    /// </param>
+    /// <returns>
+    ///     The newly created gameobject.
+    /// </returns>
     public GameObject AddIcon(string name, Sprite sprite, Vector3 position, float rotation, float scale)
     {
         GameObject mapIcon = new(name);
@@ -67,7 +104,15 @@ public class MiniMap : MonoBehaviour
         return mapIcon;
     }
 
-    // -0.5 <= posX,Y <= 0.5
+    /// <param name="posX">
+    ///     The horizontal position between -0.5 and 0.5.
+    /// </param>
+    /// <param name="posY">
+    ///     The vertical position between -0.5 and 0.5.
+    /// </param>
+    /// <returns>
+    ///     The given (x,y) position on the minimap as a local position.
+    /// </returns>
     private Vector3 MapPosition(float posX = 0, float posY = 0)
     {
         Rect mapRect = rectTransform.rect;
