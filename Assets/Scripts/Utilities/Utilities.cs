5610ae20f46a21d2d30db4ce861ba4bfd0e663ea
MaxFlorescence <maxflorescence@gmail.com>
2026-05-03 22:23:18 -0600

    Separate AudioSource extension into its own file.

--- Assets/Scripts/Utilities/Utilities.cs
+++ Assets/Scripts/Utilities/Utilities.cs
@@ -9,26 +9,6 @@ using Unity.Mathematics;
 
 public static class Extensions
 {
-    /// <summary>
-    ///     Plays an AudioClip, and scales the AudioSource pitch randomly between the given bounds.
-    /// </summary>
-    /// <param name="audioClip">
-    ///     The clip to play.
-    /// </param>
-    /// <param name="pitchBounds">
-    ///     The minimum and maximum pitches that the AudioClip can play at.
-    /// </param>
-    public static void PlayRandomPitchOneShot(this AudioSource audioSource, AudioClip audioClip, Vector2? pitchBounds = null)
-    {
-        if (audioClip == null) return;
-        
-        float originalPitch = audioSource.pitch;
-        audioSource.pitch = Random.Range(pitchBounds?.x ?? 1, pitchBounds?.y ?? 1);
-
-        audioSource.PlayOneShot(audioClip);
-        
-        audioSource.pitch = originalPitch;
-    }
 
     public static T Clone<T>(this T cloneMe) where T : struct
     {
